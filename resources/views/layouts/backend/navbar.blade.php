ed7dfa76f9f07ddfadfd4eeb73f2d2f1606d80ff
root <yahya.left@gmail.com>
2018-09-19 07:00:25 +0700

    changes by delineate

--- resources/views/layouts/backend/navbar.blade.php
+++ resources/views/layouts/backend/navbar.blade.php
@@ -62,17 +62,17 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="/image/{{ App\User::find($currentUser->id)->image }}" class="user-image" alt="{{ $currentUser->name }}">
-              <span class="hidden-xs">Hai, {{ $currentUser->name }}</span>
+              <img src="{{ Avatar::create($currentUser->name)->toBase64() }}" class="user-image" alt="{{ $currentUser->name }}">
+              <span class="hidden-xs">{{ $currentUser->name }}</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="/image/{{ App\User::find($currentUser->id)->image }}" />
+                <img src="{{ Avatar::create($currentUser->name)->toBase64() }}" />
 
                 <p>
                   <?php $userRole = $currentUser->roles->first() !== NULL ? $currentUser->roles->first()->display_name : "Noob" ?>
-
+                  
                   {{ $currentUser->name }} - {{$userRole}}
                   <!-- <small>Member since Nov. 2012</small> -->
                 </p>
