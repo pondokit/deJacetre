33142f2c25ad6ca8de123068622d5c7b02d9d3e7
masebilal07 <masebilal2307@gmail.com>
2018-09-21 20:20:55 +0700

    contact

---
+++ database/seeds/ContactTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ContactTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('contact')->truncate();
+
+      DB::table('contact')->insert([
+       [
+         'id'		=> 1,
+         'nama'	=> 'Pondok Informatika Al Madinah',
+         'telepon'	=> '+62 85378456213',
+         'email'	=> 'adminweb@gmail.com',
+         'alamat'	=> 'jl. krapyak deket masjid widi khasanan',
+       ],
+      ]);
+    }
+}
